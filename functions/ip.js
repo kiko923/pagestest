9de61ee3de0643b2d4f975bb27a45e622210d4cd
kiko923 <75443014+kiko923@users.noreply.github.com>
2025-01-26 23:24:11 +0800

    Update ip.js

--- functions/ip.js
+++ functions/ip.js
@@ -1,18 +1,9 @@
 export async function onRequest(context) {
     const { request, env } = context;
-    const ip = request.headers.get("x-forwarded-for") || request.headers.get("clientIP")
-    const asOrganization = request.cf.asOrganization || ""
-    return Response.json({
-        "ip": ip,
-        "asOrganization": asOrganization
-    }, {
-        headers: {
-            "content-type": "application/json;charset=UTF-8",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": "true",
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Methods": "*",
-            "ip": ip
-        },
-    });
+
+    // 生成一个0到1000之间的随机整数
+    const randomNumber = Math.floor(Math.random() * 1000);
+
+    // 返回随机数字
+    return new Response(randomNumber.toString());
 }
